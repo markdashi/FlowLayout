e1d751a104d4ddf9af9396c387cec21cb04eee9b
Apple <apple@appletekimacbook-pro.local>
2016-01-15 18:43:52 +0800

    初始化collectionViewFlowLayout

---
+++ FlowLayout/FlowLayout/waterFlowlayout.h
@@ -0,0 +1,13 @@
+//
+//  waterFlowlayout.h
+//  FlowLayout
+//
+//  Created by Apple on 16/1/15.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface waterFlowlayout : UICollectionViewLayout
+
+@end
---
+++ FlowLayout/FlowLayout/waterFlowlayout.m
@@ -0,0 +1,56 @@
+//
+//  waterFlowlayout.m
+//  FlowLayout
+//
+//  Created by Apple on 16/1/15.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "waterFlowlayout.h"
+
+@implementation waterFlowlayout
+
+//实现自定义布局实现4种方法
+/**
+ *  初始化
+ */
+- (void)prepareLayout
+{
+    [super prepareLayout];
+
+}
+
+/**
+ *  决定cell的排布
+ */
+- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect
+{
+
+    //创建
+    
+    
+    
+    return nil;
+}
+
+/**
+ *  返回indexPath 位置cell对应的布局属性
+ */
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+/**
+ *  collectionview滚动返回CGSize
+*/
+- (CGSize)collectionViewContentSize
+{
+    return CGSizeMake(0, 1000);
+}
+
+
+
+
+
+@end
